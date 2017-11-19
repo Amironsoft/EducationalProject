dd5e25173a6b275f25abf99caeaf458b56919591
o <amironsoft@mail.ru>
2017-11-19 22:24:25 +0300

    add status img

--- app/src/main/java/ru/ifmo/fablab/myapplication/MainActivity.java
+++ app/src/main/java/ru/ifmo/fablab/myapplication/MainActivity.java
@@ -5,22 +5,31 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 import ru.ifmo.fablab.myapplication.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ViewModel.ErrorListener{
 
     @BindingAdapter({"app:url"})
-    public static void bindImageUrl(ImageView imageView, String url){
-        Picasso.with(imageView.getContext()).load(url).into(imageView);
+    public static void bindImageUrl(ImageView imageView, String url) {
+        Picasso.with(imageView.getContext())
+                .load(url)
+                .error(R.drawable.ic_launcher_background)
+                .into(imageView);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
-        binding.setVm(new ViewModel("https://vignette.wikia.nocookie.net/fantendo/images/f/fa/Mario_NASASR.png"));
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setVm(new ViewModel("https://vignette.wikia.nocookie.net/fantendo/images/f/fa/Mario_NASASR.png", this));
+    }
+
+    @Override
+    public void onError(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/ru/ifmo/fablab/myapplication/ViewModel.java
+++ app/src/main/java/ru/ifmo/fablab/myapplication/ViewModel.java
@@ -1,22 +1,60 @@
 package ru.ifmo.fablab.myapplication;
 
+import android.databinding.ObservableField;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Observable;
+import java.util.regex.Pattern;
+
 /**
  * Created by mlebedev on 04/11/2017.
  */
 
 public class ViewModel {
-
-    private String url;
-
-    public ViewModel(String url) {
-        this.url = url;
+    private final static Pattern IMAGE_PATTERN = Pattern.compile("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
+    public ObservableField<String> url;
+    interface ErrorListener {
+        void onError(String message);
     }
 
-    public String getUrl() {
-        return url;
+    private final ErrorListener errorListener;
+
+    public String getInput() {
+        return input;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
+    public void setInput(String input) {
+        this.input = input;
+
+//        if (input != null && !input.isEmpty() && IMAGE_PATTERN.matcher(input).find()){
+//            url.set(input);
+//        }
     }
+
+    private String input;
+
+    public ViewModel(String url, ErrorListener errorListener) {
+        this.url = new ObservableField<>(url);
+        this.input = url;
+        this.errorListener = errorListener;
+    }
+
+    public void refreshClick(View view) {
+        Log.i("TEST", "Refresh click");
+        if (input != null && !input.isEmpty() && IMAGE_PATTERN.matcher(input).find()) {
+            url.set(input);
+        } else {
+            errorListener.onError("Error");
+        }
+    }
+
+//    public ObservableField<String> getUrl() {
+//        return url;
+//    }
+//
+//    public void setUrl(String url) {
+//        this.url = url;
+//    }
 }
